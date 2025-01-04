26e9ebb2f93bf8d3a04bf4bf0190590b96d2c30b
qPels01 <petrlensin011@gmail.com>
2025-01-04 19:33:26 +0300

    Подготовлена рабочая область

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const helmet = require('helmet');
+const cors = require('cors')
+const app = express()
+
+app.use(cors({
+    host: 'http://localhost:5000',
+    origin: 'http://localhost:3001', 
+    methods: 'GET, POST, PUT, DELETE, OPTIONS', 
+    allowedHeaders: ['Authorization', 'refresh_token', 'Content-Type'],
+    credentials: true 
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+  app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            "default-src": ["'self'"],
+            "script-src": ["'self'", "'unsafe-inline'"],
+            "style-src": ["'self'", "'unsafe-inline'"],
+            "img-src": ["'self'", "data:", "https:"],
+            "connect-src": ["'self'", "http://localhost:5000"],
+        },
+    },
+  }));
+
+  app.use(helmet.noSniff())
+
+app.use('/auth', require("./routers/auth"))
+app.use('/user', require("./routers/user"))
+app.use('/friends', require("./routers/friends"))
+
+app.listen(5000, (err) => {
+  if (err) {
+    console.error("Ошибка при запуске сервера:", err);
+  } else {
+    console.log('Готов на порту 5000');
+  }
+});
+
